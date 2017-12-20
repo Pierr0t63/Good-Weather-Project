68526a98db4f21eee943d4bd798143a8ba99e5d7
BlackSpirit03 <d.leger@aformac-vichy.fr>
2017-12-20 12:07:39 +0100

    Update after test of template
    Update after test of template

---
+++ goodweather/resources/views/footer.blade.php
@@ -0,0 +1,11 @@
+@extends('template')
+
+@section('footer')
+<!--Copyright-->
+<div class="footer-copyright justify-content-center text-center">
+    <div class="container-fluid text-white">
+        Propulsé par <i class="fa fa-sun-o" aria-hidden="true"></i>
+    </div>
+</div>
+<!--/.Copyright-->
+@endsection
---
+++ goodweather/resources/views/header.blade.php
@@ -0,0 +1,21 @@
+@extends('template')
+
+@section('header')
+<header class="container-fluid justify-content-center text-center">
+	
+        <img src="img/logo.png">
+
+        <div class="row justify-content-center text-center">
+
+            <form class="form-inline active-white-2 col-4">
+                <input class="form-control w-100 text-white" type="text" placeholder="Renseignez votre ville, code postal..." aria-label="Search" title="Renseignez votre position">
+            </form>
+
+            <button type="button" class="btn btn-outline-warning waves-effect px-3"><i class="fa fa-rocket" aria-hidden="true" title="Cliquez-ici pour vous géolocaliser"></i></button>`
+
+        </div>
+            
+        <button type="button" class="btn btn-outline-white waves-effect col-lg-2 col-md-2 col-sm-2 col-xs-2"><i class="fa fa-sun-o pr-2" aria-hidden="true"></i>Valider</button>
+
+</header>
+@endsection
