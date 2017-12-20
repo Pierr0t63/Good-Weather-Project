0fd583994f607972df3d28be96e1de5d6296cc20
BlackSpirit03 <d.leger@aformac-vichy.fr>
2017-12-20 15:24:19 +0100

    Update
    Update

--- goodweather/app/Http/Controllers/HomeController.php
+++ goodweather/app/Http/Controllers/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
 {
     public function index()
     {
+        return view ("template");  
     }
 
     public function maMeteo(request $request)
@@ -29,6 +30,7 @@ class HomeController extends Controller
         $owm->setApiKey('e469e4ae90d47fcdf9df7c2666e35487');
 
         $forecast = $owm->getWeatherForecast($city.',fr', $units, $lang, '', 5);
+        $weather_city = $owm->getWeather($city.',fr', $units, $lang);
 
         // Températures
         $temperatures = \Lava::DataTable();
@@ -51,18 +53,24 @@ class HomeController extends Controller
         }
         \Lava::LineChart('Temps', $temperatures, [
             'title' => 'Températures à '.$forecast->city->name,
+            'width' => '100%',
+            'height'=> '250',
             'titleTextStyle' => [
                'color'    => '#FF0000 ',
                'fontSize' => 24
         ]]);
         \Lava::LineChart('Vents', $winds, [
             'title' => 'Vent à '.$forecast->city->name,
+            'width' => '100%',
+            'height'=> '250',
             'titleTextStyle' => [
                'color'    => '#FF0000 ',
                'fontSize' => 24
         ]]);
         \Lava::LineChart('Humidity', $humidity, [
             'title' => 'Humidité de l\'air à '.$forecast->city->name,
+            'width' => '100%',
+            'height'=> '250',
             'titleTextStyle' => [
                'color'    => '#FF0000 ',
                'fontSize' => 24
@@ -70,5 +78,6 @@ class HomeController extends Controller
 
         Mapper::map($forecast->city->lat, $forecast->city->lon);
 
+		return view("template", compact('weather_city'));
 	}
 }
