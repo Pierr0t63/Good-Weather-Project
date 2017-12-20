4621f261c4d9a16077a590ed05aa8cab304017db
Benouk <32901416+BNK313@users.noreply.github.com>
2017-12-20 16:30:42 +0100

    int graph + tabs

--- goodweather/resources/views/charts.blade.php
+++ goodweather/resources/views/charts.blade.php
@@ -1,13 +1,39 @@
-            <section id="divTemp" class="container-fluid">
+<!-- Nav tabs -->
+<ul class="nav nav-tabs nav-justified blue lighten-5 container-fluid mt-5 justify-content-center ml-4" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link active" data-toggle="tab" href="#panel5" role="tab"><i class="fa fa-thermometer-empty"></i>  Température</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#panel6" role="tab"><i class="fa fa-paper-plane-o"></i> Vent</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#panel7" role="tab"><i class="fa fa-tint"></i> Humidité</a>
+    </li>
+</ul>
+<!-- Tab panels -->
+<div class="tab-content container-fluid">
+    <!--Panel 1-->
+    <div class="tab-pane fade in show active" id="panel5" role="tabpanel">
+        <br>
+        <section id="divTemp" class="container-fluid">
                 @linechart('Temps','divTemp')
             </section>
-            
-
-            <section id="divWind" class="container-fluid">
+    </div>
+    <!--/.Panel 1-->
+    <!--Panel 2-->
+    <div class="tab-pane fade" id="panel6" role="tabpanel">
+        <br>
+         <section id="divWind" class="container-fluid">
                 @linechart('Vents','divWind')
             </section>
-            
-
-            <section id="divHumidity" class="container-fluid">
+    </div>
+    <!--/.Panel 2-->
+    <!--Panel 3-->
+    <div class="tab-pane fade" id="panel7" role="tabpanel">
+        <br>
+        <section id="divHumidity" class="container-fluid">
                 @linechart('Humidity','divHumidity')
             </section>
+    </div>
+    <!--/.Panel 3-->
+</div>
