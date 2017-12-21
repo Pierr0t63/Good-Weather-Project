71076592ad10317116fb580004feea793cf90b65
BlackSpirit03 <d.leger@aformac-vichy.fr>
2017-12-21 13:24:26 +0100

    Update for Autocomplete Tests
    Update for Autocomplete Tests

--- goodweather/public/js/script.js
+++ goodweather/public/js/script.js
@@ -1,24 +1,96 @@
 $('document').ready( function(){
 
+
+    // Controle du champ
+    // $('#posUser').on('input',function(){
+
+    //     if( $('#posUser').val().length>=3 ) {
+
+
+    //         initializeAutocomplete('posUser');
+    //     }
+
+    // });
+
+    //Autocompletion
+    // function initializeAutocomplete(id) {
+    //     var options = { 
+    //         types: ['(cities)'],
+    //         //componentRestrictions: {country: 'fr'},
+    //     };
+
+    //     var element = document.getElementById(id);
+
+    //     if (element) {
+    //         var autocomplete = new google.maps.places.Autocomplete(element, options); 
+    //     }
+
+    //     autocomplete.addListener('Place_changed',fillInAddress);
+    // };
+
+      var placeSearch, autocomplete;
+      var componentForm = {
+        //street_number: 'short_name',
+        //route: 'long_name',
+        locality: 'long_name',
+        //administrative_area_level_1: 'short_name',
+        country: 'short_name',
+        //postal_code: 'short_name'
+      };
+
+    // Controle du champ 2
     $('#posUser').on('input',function(){
 
         if( $('#posUser').val().length>=3 ) {
-            
-            initializeAutocomplete('posUser');
+
+            var options = { 
+                types: ['geocode'],
+                // componentRestrictions: {country: 'fr'},
+            };
+
+            var element = document.getElementById('posUser');
+
+            if (element) {
+                autocomplete = new google.maps.places.Autocomplete(element, options); 
+            }
+
+            autocomplete.addListener('Place_changed',fillInAddress);
 
         }
 
     });
 
-    function initializeAutocomplete(id) {
-        var options = { 
-            types: ['(cities)'],
-            componentRestrictions: {country: 'fr'},
-        };
-        var element = document.getElementById(id);
-        if (element) {
-            var autocomplete = new google.maps.places.Autocomplete(element, options);
+    $('#posUser').on('clic',function(){
+
+            alert("toto");
+
+    });
+
+
+    //Filtre final
+      function fillInAddress() {
+        
+        // Get the place details from the autocomplete object.
+        var place = autocomplete.getPlace();
+
+        for (var component in componentForm) {
+          document.getElementById(component).value = '';
+          document.getElementById(component).disabled = false;
         }
-    };
+
+        // Get each component of the address from the place details
+        // and fill the corresponding field on the form.
+        document.getElementById('posUser').value = "";
+
+        for (var i = 0; i < place.address_components.length; i++) {
+          var addressType = place.address_components[i].types[0];
+          if (componentForm[addressType]) {
+            var val = place.address_components[i][componentForm[addressType]];
+            document.getElementById('posUser').value += val + ",";
+          }
+        }
+        
+      }
+
 
 });
